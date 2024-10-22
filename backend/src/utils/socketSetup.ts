6d724c339969e5f1219b395f3f6376e1b862ee29
theo <theo.paraskeva123@gmail.com>
2024-10-22 01:44:55 +0100

    revert

--- backend/src/utils/socketSetup.ts
+++ backend/src/utils/socketSetup.ts
@@ -1,7 +1,11 @@
 import { MyContext } from "..";
-import { ClientToServerEvents, ExtendedIncomingMessage, ServerToClientEvents } from "../types/SocketTypes";
+import {
+  ClientToServerEvents,
+  ExtendedIncomingMessage,
+  ServerToClientEvents,
+} from "../types/SocketTypes";
 import { Server, Socket } from "socket.io";
-import { Server as HttpServer } from 'http';
+import { Server as HttpServer } from "http";
 import { checkCrosswordCompletion } from "./checkComplete";
 
 export function setupSocketIO(
@@ -9,219 +13,237 @@ export function setupSocketIO(
   sessionMiddleware: any,
   context: MyContext
 ) {
-  console.log('Setting up Socket.IO');
-  const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
-    cors: {
-      origin: 'http://localhost:5173',
-      credentials: true,
-    },
-  });
+  console.log("Setting up Socket.IO");
+  const io = new Server<ClientToServerEvents, ServerToClientEvents>(
+    httpServer,
+    {
+      cors: {
+        origin: "http://localhost:5173",
+        credentials: true,
+      },
+    }
+  );
 
   io.engine.use(sessionMiddleware);
 
-  context.redis.config('SET', 'notify-keyspace-events', 'Ex');
+  context.redis.config("SET", "notify-keyspace-events", "Ex");
 
   const subscriber = context.redis.duplicate();
-  subscriber.psubscribe('__keyevent@*__:expired');
+  subscriber.psubscribe("__keyevent@*__:expired");
 
-  subscriber.on('pmessage', async (pattern, channel, expiredKey) => {
-    if (expiredKey.startsWith('game:')) {
-      const gameId = expiredKey.split(':')[1];
-      
-      io.to(`game:${gameId}`).emit('gameExpired', {
+  subscriber.on("pmessage", async (pattern, channel, expiredKey) => {
+    if (expiredKey.startsWith("game:")) {
+      const gameId = expiredKey.split(":")[1];
+
+      io.to(`game:${gameId}`).emit("gameExpired", {
         gameId,
-        message: 'The game has expired due to inactivity.',
+        message: "The game has expired due to inactivity.",
       });
-
-      const gameStateJson = await context.redis.get(expiredKey);
-    if (gameStateJson) {
-      const gameState = JSON.parse(gameStateJson);
-    
-
-      await Promise.all(
-        gameState.players.map((playerId: string) => context.redis.del(`user:${playerId}:activeGame`))
-      );
     }
-    }
-   
   });
 
-  io.on('connection', (socket: Socket<ClientToServerEvents, ServerToClientEvents>) => {
-    console.log('Client connected:', socket.id);
+  io.on(
+    "connection",
+    (socket: Socket<ClientToServerEvents, ServerToClientEvents>) => {
+      console.log("Client connected:", socket.id);
 
-    const req = socket.request as ExtendedIncomingMessage;
-    const session = req.session;
-    const userId = session.userId;
+      const req = socket.request as ExtendedIncomingMessage;
+      const session = req.session;
+      const userId = session.userId;
 
-    if (!userId) {
-      console.log('Unauthenticated socket connection. Disconnecting.');
-      socket.disconnect(true);
-      return;
-    }
+      if (!userId) {
+        console.log("Unauthenticated socket connection. Disconnecting.");
+        socket.disconnect(true);
+        return;
+      }
 
-    console.log('User connected:', userId);
+      console.log("User connected:", userId);
 
-    socket.join(`user:${userId}`);
+      socket.join(`user:${userId}`);
 
-    socket.on('joinGame', async ({ gameId }) => {
-      try {
-        const userGameKey = `user:${userId}:activeGame`;
-        const activeGameId = await context.redis.get(userGameKey);
+      socket.on("joinGame", async ({ gameId }) => {
+        try {
+          const userGameKey = `user:${userId}:activeGame`;
 
-        if (activeGameId && activeGameId !== gameId) {
-          socket.emit('redirectToGame', { gameId: activeGameId });
-          return;
-        }
-
-        const gameKey = `game:${gameId}`;
-        const gameStateJson = await context.redis.get(gameKey);
-        if (!gameStateJson) {
-          socket.emit('error', { message: 'Game not found or has expired due to inactivity.' });
-          return;
-        }
-
-        const gameState = JSON.parse(gameStateJson);
-
-        if (!gameState.players.includes(userId)) {
-          if (gameState.players.length >= 2) {
-            socket.emit('error', { message: 'Game is full' });
+          const gameKey = `game:${gameId}`;
+          const gameStateJson = await context.redis.get(gameKey);
+          if (!gameStateJson) {
+            socket.emit("error", {
+              message: "Game not found or has expired due to inactivity.",
+            });
             return;
           }
 
-          gameState.players.push(userId);
-          await context.redis.set(gameKey, JSON.stringify(gameState), 'KEEPTTL');
-        }
+          const gameState = JSON.parse(gameStateJson);
 
-        socket.join(`game:${gameId}`);
+          if (!gameState.players.includes(userId)) {
+            if (gameState.players.length >= 2) {
+              socket.emit("error", { message: "Game is full" });
+              return;
+            }
 
-        if (!gameState.playerStates[userId]) {
-          const playerPuzzle = gameState.puzzle.map((cell: any) => ({
-            isBlack: cell.isBlack,
-            position: cell.position,
-            clues: cell.clues,
-            value: '', 
-          }));
-
-          gameState.playerStates[userId] = { puzzle: playerPuzzle };
-          await context.redis.set(gameKey, JSON.stringify(gameState), 'KEEPTTL');
-        }
-
-        await context.redis.set(userGameKey, gameId);
-
-        const clientGameState = {
-          gameId: gameState.gameId,
-          clues: gameState.clues,
-          dim: gameState.dim,
-          status: gameState.status,
-          puzzle: gameState.playerStates[userId].puzzle,
-        };
-
-        socket.emit('gameState', clientGameState);
-
-        if (gameState.players.length === 2 && gameState.status === 'waiting') {
-          gameState.status = 'ongoing';
-          await context.redis.set(gameKey, JSON.stringify(gameState), 'KEEPTTL');
-          await context.redis.expire(gameKey, 1800); 
-
-          io.to(`game:${gameId}`).emit('gameStarted', { gameId });
-        }
-      } catch (error) {
-        console.error('Error joining game:', error);
-        socket.emit('error', { message: 'An error occurred while joining the game.' });
-      }
-    });
-
-    socket.on('makeMove', async ({ gameId, move }) => {
-      try {
-        const gameKey = `game:${gameId}`;
-        const gameStateJson = await context.redis.get(gameKey);
-        if (!gameStateJson) {
-          socket.emit('error', { message: 'Game not found or has expired.' });
-          return;
-        }
-
-        const gameState = JSON.parse(gameStateJson);
-
-        if (gameState.status !== 'ongoing') {
-          socket.emit('error', { message: 'Waiting for players' });
-          return;
-        }
-
-        if (!gameState.players.includes(userId)) {
-          socket.emit('error', { message: 'You are not a player in this game' });
-          return;
-        }
-
-        if (!gameState.playerStates[userId]) {
-          socket.emit('error', { message: 'Player state not found.' });
-          return;
-        }
-
-        const playerPuzzle = gameState.playerStates[userId].puzzle;
-        const cellIndex = playerPuzzle.findIndex(
-          (cell: any) =>
-            cell.position.x === move.position.x && cell.position.y === move.position.y
-        );
-
-        if (cellIndex !== -1) {
-          playerPuzzle[cellIndex].value = move.value;
-
-          await context.redis.set(gameKey, JSON.stringify(gameState), "KEEPTTL");
-
-          const isComplete = checkCrosswordCompletion(gameState, userId);
-
-          if (isComplete) {
-            gameState.status = 'ended';
-            await context.redis.set(gameKey, JSON.stringify(gameState), "KEEPTTL");
-
-            io.to(`game:${gameId}`).emit('gameEnded', {
-              gameId,
-              winner: userId,
-            });
-
-            await context.prisma.game.create({
-              data: {
-                id: gameId,
-                gameState: gameState,
-                status: gameState.status,
-                winnerId: userId,
-                users: {
-                  connect: gameState.players.map((playerId: string) => ({ id: playerId })),
-                },
-                finishedStates: {
-                  create: gameState.players.map((playerId: string) => ({
-                    userId: playerId,
-                    state: gameState.playerStates[playerId],
-                  })),
-                },
-              },
-            });
-
-            await Promise.all(
-              gameState.players.map((playerId: string) => context.redis.del(`user:${playerId}:activeGame`))
+            gameState.players.push(userId);
+            await context.redis.set(
+              gameKey,
+              JSON.stringify(gameState),
+              "KEEPTTL"
             );
-            
-
-            await context.redis.del(gameKey);
-          } else {
-            socket.emit('updatePlayerState', {
-              gameId,
-              puzzle: playerPuzzle,
-            });
           }
-        } else {
-          socket.emit('error', { message: 'Invalid cell position' });
-        }
-      } catch (error) {
-        console.error('Error handling makeMove:', error);
-        socket.emit('error', { message: 'An error occurred while processing your move.' });
-      }
-    });
 
-    socket.on('disconnect', () => {
-      console.log('User disconnected:', userId);
-    });
-  });
+          socket.join(`game:${gameId}`);
+
+          if (!gameState.playerStates[userId]) {
+            const playerPuzzle = gameState.puzzle.map((cell: any) => ({
+              isBlack: cell.isBlack,
+              position: cell.position,
+              clues: cell.clues,
+              value: "",
+            }));
+
+            gameState.playerStates[userId] = { puzzle: playerPuzzle };
+            await context.redis.set(
+              gameKey,
+              JSON.stringify(gameState),
+              "KEEPTTL"
+            );
+          }
+
+          await context.redis.set(userGameKey, gameId);
+
+          const clientGameState = {
+            gameId: gameState.gameId,
+            clues: gameState.clues,
+            dim: gameState.dim,
+            status: gameState.status,
+            puzzle: gameState.playerStates[userId].puzzle,
+          };
+
+          socket.emit("gameState", clientGameState);
+
+          if (
+            gameState.players.length === 2 &&
+            gameState.status === "waiting"
+          ) {
+            gameState.status = "ongoing";
+            await context.redis.set(
+              gameKey,
+              JSON.stringify(gameState),
+              "KEEPTTL"
+            );
+            await context.redis.expire(gameKey, 1800);
+
+            io.to(`game:${gameId}`).emit("gameStarted", { gameId });
+          }
+        } catch (error) {
+          console.error("Error joining game:", error);
+          socket.emit("error", {
+            message: "An error occurred while joining the game.",
+          });
+        }
+      });
+
+      socket.on("makeMove", async ({ gameId, move }) => {
+        try {
+          const gameKey = `game:${gameId}`;
+          const gameStateJson = await context.redis.get(gameKey);
+          if (!gameStateJson) {
+            socket.emit("error", { message: "Game not found or has expired." });
+            return;
+          }
+
+          const gameState = JSON.parse(gameStateJson);
+
+          if (gameState.status !== "ongoing") {
+            socket.emit("error", { message: "Waiting for players" });
+            return;
+          }
+
+          if (!gameState.players.includes(userId)) {
+            socket.emit("error", {
+              message: "You are not a player in this game",
+            });
+            return;
+          }
+
+          if (!gameState.playerStates[userId]) {
+            socket.emit("error", { message: "Player state not found." });
+            return;
+          }
+
+          const playerPuzzle = gameState.playerStates[userId].puzzle;
+          const cellIndex = playerPuzzle.findIndex(
+            (cell: any) =>
+              cell.position.x === move.position.x &&
+              cell.position.y === move.position.y
+          );
+
+          if (cellIndex !== -1) {
+            playerPuzzle[cellIndex].value = move.value;
+
+            await context.redis.set(
+              gameKey,
+              JSON.stringify(gameState),
+              "KEEPTTL"
+            );
+
+            const isComplete = checkCrosswordCompletion(gameState, userId);
+
+            if (isComplete) {
+              gameState.status = "ended";
+              await context.redis.set(
+                gameKey,
+                JSON.stringify(gameState),
+                "KEEPTTL"
+              );
+
+              io.to(`game:${gameId}`).emit("gameEnded", {
+                gameId,
+                winner: userId,
+              });
+
+              await context.prisma.game.create({
+                data: {
+                  id: gameId,
+                  gameState: gameState,
+                  status: gameState.status,
+                  winnerId: userId,
+                  users: {
+                    connect: gameState.players.map((playerId: string) => ({
+                      id: playerId,
+                    })),
+                  },
+                  finishedStates: {
+                    create: gameState.players.map((playerId: string) => ({
+                      userId: playerId,
+                      state: gameState.playerStates[playerId],
+                    })),
+                  },
+                },
+              });
+
+              await context.redis.del(gameKey);
+            } else {
+              socket.emit("updatePlayerState", {
+                gameId,
+                puzzle: playerPuzzle,
+              });
+            }
+          } else {
+            socket.emit("error", { message: "Invalid cell position" });
+          }
+        } catch (error) {
+          console.error("Error handling makeMove:", error);
+          socket.emit("error", {
+            message: "An error occurred while processing your move.",
+          });
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log("User disconnected:", userId);
+      });
+    }
+  );
 
   return io;
 }
